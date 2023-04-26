ef3a3baebeeb39dc8c3c4ea633400a8f8fe7c76e
Gmij <gmij@qq.com>
2023-04-26 23:06:50 +0800

    add. 把所有开启双缓冲的控件全放在这个类里了。

---
+++ DoubleBufferControl.cs
@@ -0,0 +1,26 @@
+﻿namespace DynamicWallpaper
+{
+    public class DoubleBufferPanel : Panel
+    {
+        public DoubleBufferPanel()
+        {
+            DoubleBuffered = true;
+        }
+    }
+
+    public class DoubleBufferFlowPanel : FlowLayoutPanel
+    {
+        public DoubleBufferFlowPanel()
+        {
+            DoubleBuffered = true;
+        }
+    }
+
+    public class DoubleBufferPictureBox : PictureBox
+    {
+        public DoubleBufferPictureBox()
+        {
+            DoubleBuffered = true;
+        }
+    }
+}
