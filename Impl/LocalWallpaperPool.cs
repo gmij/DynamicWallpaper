a14faba9177600dedbe30072b99c1876e5f9c923
Gmij <gmij@qq.com>
2023-04-26 16:56:13 +0800

    update.重构本地壁纸池

--- Impl/LocalWallpaperPool.cs
+++ Impl/LocalWallpaperPool.cs
@@ -1,148 +1,160 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.Extensions.Logging;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 using Timer = System.Threading.Timer;
 
 namespace DynamicWallpaper.Impl
 {
-    internal class LocalWallpaperPool : IWallPaperPool
+
+
+    class LocalWallpaperPool : IWallPaperPool
     {
-        private readonly string _cachePath;
-        private string[] _wallPaperList;
-        private readonly ILogger<LocalWallpaperPool> _logger;
-        private FileSystemWatcher _watcher;
+        static object locker = new object();
+        private IList<WallpaperPreview> previews;
+        private readonly ILogger<LocalWallpaperPool> logger;
 
-        private static object locker = new object();
-        private static DateTime _lastUpdateTime = DateTime.Now;
+        private FileSystemWatcher watcher;
+        private string cachePath;
 
-        private static int _refreshTimeSpan = 2000;
-
-        public bool IsEmpty => _wallPaperList.Length == 0;
-
-        private EventHandler<int> _wallPaperChanged;
-        private Timer _refreshTask;
-
-        public EventHandler<int> WallPaperChanged { get => _wallPaperChanged; set => _wallPaperChanged = value; }
 
         public LocalWallpaperPool(WallpaperSetting setting, ILogger<LocalWallpaperPool> logger)
         {
-            _cachePath = setting.CachePath;
-            _wallPaperList = new string[0];
-            InitPaperPool();
-            _logger = logger;
-            
-            _watcher = new FileSystemWatcher(_cachePath);
+            cachePath = setting.CachePath;
+            var files = Directory.GetFiles(cachePath, "*", SearchOption.AllDirectories);
+            previews = new List<WallpaperPreview>();
+            LoadImages(files);
+            this.logger = logger;
+            watcher = new FileSystemWatcher(cachePath);
             InitWatcher();
         }
 
-
         private void InitWatcher()
         {
-            _watcher.IncludeSubdirectories = true;
-            _watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.CreationTime | NotifyFilters.Size;
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.CreationTime | NotifyFilters.Size;
 
-            _watcher.Created += FileChanged;
-            _watcher.Renamed += FileChanged;
-            _watcher.Deleted += FileChanged;
+            watcher.Created += FileChanged;
+            watcher.Renamed += FileChanged;
+            watcher.Deleted += FileChanged;
 
-            _watcher.EnableRaisingEvents = true;
-
-            _refreshTask = new Timer(FireFileChangedEvent, null, Timeout.Infinite, Timeout.Infinite);
-
+            watcher.EnableRaisingEvents = true;
         }
 
         private void FileChanged(object sender, FileSystemEventArgs e)
         {
-            _logger.LogInformation($"文件发生变化：{e.FullPath}, {e.ChangeType}");
-            ReLoadCacheImage();
-            //_logger.LogInformation($"文件发生变化：刷新缓存");
-
-            //  通过定时器滑动，减少事件的激活次数。
-            _logger.LogInformation($"文件发生变化：定时器滑动");
-            _refreshTask.Change(_refreshTimeSpan, Timeout.Infinite);
-        }
-
-        private void FireFileChangedEvent(object? state)
-        {
-            WallPaperChanged.Invoke(null, _wallPaperList.Length);
-            _refreshTask.Change(Timeout.Infinite, Timeout.Infinite);
-            _logger.LogInformation($"文件发生变化事件已抛出：定时器禁用");
-        }
-
-        private void InitPaperPool()
-        {
-            _wallPaperList = Directory.GetFiles(_cachePath, "*", SearchOption.AllDirectories);
-            _lastUpdateTime = DateTime.Now;
-        }
-
-
-        private void ReLoadCacheImage()
-        {
-            if (!string.IsNullOrEmpty(_cachePath))
+            WallpaperPreview? item = null;
+            lock (locker)
             {
-                if (Directory.Exists(_cachePath))
+                switch (e.ChangeType)
                 {
-                    if (DateTime.Now - _lastUpdateTime > TimeSpan.FromSeconds(2))
-                    {
-                        lock (locker)
+                    case WatcherChangeTypes.Created:
+                        item = previews.FirstOrDefault(p => p.Path == e.FullPath);
+                        if (item != null)
                         {
-                            if (DateTime.Now - _lastUpdateTime > TimeSpan.FromSeconds(2))
-                            {
-                                InitPaperPool();
-                            }
+                            logger.LogWarning("文件已存在:{0}", e.FullPath);
                         }
-                    }
-                }
-                else
-                {
-                    Directory.CreateDirectory(_cachePath);
+                        else
+                        {
+                            logger.LogInformation("文件已添加:{0}", e.FullPath);
+                            item = LoadImage(e.FullPath);
+                            previews.Add(item);
+                        }
+                        break;
+                    case WatcherChangeTypes.Deleted:
+                        item = previews.FirstOrDefault(p => p.Path == e.FullPath);
+                        if (item != null)
+                        {
+                            logger.LogInformation("文件已删除:{0}", e.FullPath);
+                            previews.Remove(item);
+                        }
+                        else
+                        {
+                            logger.LogWarning("文件不存在:{0}", e.FullPath);
+                        }
+                        break;
+                    case WatcherChangeTypes.Changed:
+                    case WatcherChangeTypes.Renamed:
+                        item = previews.FirstOrDefault(p => p.Path == e.FullPath);
+                        if (item != null)
+                        {
+                            logger.LogInformation("文件已修改:{0}", e.FullPath);
+                            previews.Remove(item);
+                            item = LoadImage(e.FullPath);
+                            previews.Add(item);
+                        }
+                        else
+                        {
+                            logger.LogWarning("文件不存在:{0}", e.FullPath);
+                        }
+                        break;
                 }
             }
+            WallPaperChanged?.Invoke(this, new WallpaperChangedEventArgs(item, e.ChangeType));
+        }
+
+        private void LoadImages(string[] files)
+        {
+            lock (locker)
+            {
+                previews.Clear();
+                foreach (var file in files)
+                {
+                    if (File.Exists(file))
+                    {
+                        previews.Add(LoadImage(file));
+                    }
+                }
+            }
+        }
+
+        private WallpaperPreview LoadImage(string file)
+        {
+            // 切换一下线程，让之前的写文件句柄得到释放。
+            Thread.Sleep(1);
+            // 以下代码为Coplit 自动生成，用于解决Image.FromFile的句柄占用问题
+            using var stream = new FileStream(file, FileMode.Open, FileAccess.Read);
+            var bitmap = new Bitmap(stream);
+            // 在这里可以对bitmap进行进一步处理
+            var preview = new WallpaperPreview
+            {
+                Path = file,
+                Image = (Image)bitmap.Clone()
+            };
+            // 释放bitmap占用的内存
+            bitmap.Dispose();
+            return preview;
+        }
+
+        public bool IsEmpty => !previews.Any();
+
+        public EventHandler<WallpaperChangedEventArgs> WallPaperChanged { get; set; }
+
+        public IList<WallpaperPreview> GetWallpaperPreviews()
+        {
+            return previews;
         }
 
         public string Renew(string excludePath)
         {
             if (!IsEmpty)
             {
-                var list = _wallPaperList.Where(p => p != excludePath).ToArray();
+                var list = previews.Where(p => p.Path != excludePath).ToArray();
                 var poolSize = list.Count();
-                var file = list[new Random().Next(poolSize)];
-                if (File.Exists(file))
+                if (poolSize == 0)
                 {
-                    return file;
+                    return string.Empty;
+                }
+                var file = list[new Random().Next(poolSize)];
+                if (File.Exists(file.Path))
+                {
+                    return file.Path;
                 }
                 else
                 {
-                    _logger.LogWarning("缓存图片失效:{0}", file);
-                    ReLoadCacheImage();
+                    logger.LogWarning("缓存图片失效:{0}", file.Path);
                 }
             }
             return string.Empty;
-        }
-
-        public List<WallpaperPreview> GetWallpaperPreviews()
-        {
-            if (_wallPaperList.Length == 0)
-                ReLoadCacheImage();
-
-            var previews = new List<WallpaperPreview>();
-            foreach (var file in _wallPaperList)
-            {
-                if (File.Exists(file))
-                {
-                    // 以下代码为Coplit 自动生成，用于解决Image.FromFile的句柄占用问题
-                    using var stream = new FileStream(file, FileMode.Open, FileAccess.Read);
-                    var bitmap = new Bitmap(stream);
-                    // 在这里可以对bitmap进行进一步处理
-                    var preview = new WallpaperPreview
-                    {
-                        Path = file,
-                        Image = (Image)bitmap.Clone()
-                    };
-                    previews.Add(preview);
-                    // 释放bitmap占用的内存
-                    bitmap.Dispose();
-                }
-            }
-            return previews;
         }
     }
 }
