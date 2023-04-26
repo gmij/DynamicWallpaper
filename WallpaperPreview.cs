528e6bc71938aee8b3ec444b6bd82366e7528383
Gmij <gmij@qq.com>
2023-04-26 16:42:43 +0800

    add. 对预览图像加入ID属性，ID根据文件的磁盘物理路径来生成。

--- WallpaperPreview.cs
+++ WallpaperPreview.cs
@@ -1,7 +1,29 @@
-﻿namespace DynamicWallpaper
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace DynamicWallpaper
 {
     public class WallpaperPreview
     {
+
+        public string Id { get
+            {
+                return GetLocalHashId(Path);
+            }
+        }
+
+        public static string GetLocalHashId(string path)
+        {
+            // 把wallpaper中的path做hash，生成唯一值，存放在Id属性中，以下代码为Cursor生成
+
+            if (string.IsNullOrEmpty(path))
+                throw new ArgumentNullException(nameof(path));
+
+            using var sha256 = SHA256.Create();
+            var hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(path));
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+
 
         public string? Path { get; set; }
         public Image? Image { get; set; }
