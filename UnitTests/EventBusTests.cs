77d95bd3cc2e7f8c0b71a33579bc621e8733f09f
Gmij <gmij@qq.com>
2023-05-06 00:17:03 +0800

    变更目录结构

---
+++ UnitTests/EventBusTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DynamicWallpaper.Tests
+{
+    [TestClass]
+    public class EventBusTests
+    {
+        [TestMethod]
+        public void Register_AddsNewEventHandlerList_WhenEventNameDoesNotExist()
+        {
+            // Arrange
+            string eventName = "TestEvent";
+
+            // Act
+            EventBus.Register(eventName);
+
+            // Assert
+            Assert.IsTrue(EventBus.ContainsEvent(eventName));
+        }
+
+        [TestMethod]
+        public void Subscribe_AddsEventHandlerToList_WhenEventNameExists()
+        {
+            // Arrange
+            string eventName = "TestEvent";
+            bool eventHandled = false;
+            Action<CustomEventArgs> handler = (args) => { eventHandled = true; };
+
+            EventBus.Register(eventName);
+
+            // Act
+            EventBus.Subscribe(eventName, handler);
+            EventBus.Publish(eventName, new CustomEventArgs(null));
+
+            // Assert
+            Assert.IsTrue(eventHandled);
+        }
+
+        [TestMethod]
+        public void Subscribe_ThrowsArgumentException_WhenEventNameDoesNotExist()
+        {
+            // Arrange
+            string eventName = "NonExistentEvent";
+            Action<CustomEventArgs> handler = (args) => { };
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => EventBus.Subscribe(eventName, handler));
+        }
+
+        [TestMethod]
+        public void Publish_ThrowsArgumentException_WhenEventNameDoesNotExist()
+        {
+            // Arrange
+            string eventName = "NonExistentEvent";
+            CustomEventArgs eventArgs = new CustomEventArgs(null);
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => EventBus.Publish(eventName, eventArgs));
+        }
+    }
+}
