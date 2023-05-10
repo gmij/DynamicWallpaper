7c3872554c30920b577fd939b9d17dca7c63e192
Gmij <gmij@qq.com>
2023-05-10 22:41:05 +0800

    fixed. 如果安装在C盘下，启动时有可能没有访问权限，设置缓存目录至AppData目录

--- src/WallpaperSetting.cs
+++ src/WallpaperSetting.cs
@@ -10,14 +10,42 @@
 
         public static Size PreviewImgSize => new(220, 180);
 
-        public string CachePath => Path.Combine(LocalPath, "Cache");
+        public string CachePath { get; private set; } = Path.Combine(LocalPath, "Cache");
 
         //刷新时间(间隔几分钟更换壁纸)
         public int RefreshTime { get; set; } = 60;
 
         public string Today => DateTime.Now.ToString("yyyyMMdd");
 
-
+        public WallpaperSetting() { 
+        
+            //  Check CachePath Auth
+            if (!Directory.Exists(CachePath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(CachePath);
+                }
+                catch(UnauthorizedAccessException)
+                {
+                    var appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                    if (appDataDir != null)
+                    {
+                        var cacheDir = Path.Combine(appDataDir, "DynamicWallpaper", "Cache");
+                        if (!Directory.Exists(cacheDir))
+                        {
+                            Directory.CreateDirectory(cacheDir);
+                        }
+                        CachePath = cacheDir;
+                    }
+                    else
+                    {
+                        throw new Exception("缓存目录无法创建");
+                    }
+                }
+            }
+        
+        }
 
     }
 }
