0d89071843f590e87db902358f81812f38513884
Gmij <gmij@qq.com>
2023-06-01 22:00:49 +0800

    增加本地发布的脚本。

--- deploy.bat
+++ deploy.bat
@@ -9,3 +9,8 @@ dotnet publish %projectName% -c Release -p:PublishReadyToRun=true -p:SelfContain
 
 
 
+dotnet build MSIPackage/DynamicWallpaper.Setup.wixproj 
+
+signtool.exe sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /sha1 "079f57ca8b7f873500afed9742fa3f95b6fbc3e4"  E:\work\DynamicWallpaper.CS\MsiPackage\bin\x64\Debug\DynamicWallpaper.Setup.msi
+
+copy E:\work\DynamicWallpaper.CS\MsiPackage\bin\x64\Debug\DynamicWallpaper.Setup.msi .\bin\msi\DynamicWallpaper.Setup_SIGN.msi
